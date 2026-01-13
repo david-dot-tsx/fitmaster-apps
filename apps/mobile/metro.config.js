d96c89fbcd22e2d256c5f8aa59b3e70fe46dd614
Dawid Twardowski <davidt4323@gmail.com>
2026-01-13 19:28:06 +0100

    chore(repo): add mobile app to repo

---
+++ apps/mobile/metro.config.js
@@ -0,0 +1,18 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "../..");
+
+const config = getDefaultConfig(projectRoot);
+
+// 👇 pozwala importować z packages/*
+config.watchFolders = [workspaceRoot];
+
+// 👇 rozwiązuje node_modules z roota
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+];
+
+module.exports = config;
