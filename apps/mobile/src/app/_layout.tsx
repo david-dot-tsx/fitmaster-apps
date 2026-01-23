a7ac92f05abdc0d1caa8b8309e03b773581ccaf1
Dawid Twardowski <davidt4323@gmail.com>
2026-01-23 12:24:40 +0100

    chore(mobile-trpc-connection): setup connection, display users for example

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -2,9 +2,12 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
-import "@/global.css"
+import "@/global.css";
+
+import { ApiQueryProvider } from "@repo/api/client";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
+import { env } from "@/env";
 
 export const unstable_settings = {
   anchor: "(tabs)",
@@ -15,11 +18,13 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal", title: "Modal" }} />
-      </Stack>
-      <StatusBar style="auto" />
+      <ApiQueryProvider url={env.EXPO_PUBLIC_API_URL}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: "modal", title: "Modal" }} />
+        </Stack>
+        <StatusBar style="auto" />
+      </ApiQueryProvider>
     </ThemeProvider>
   );
 }
