14d390fd664a14fbe57825f25e701eb0b85f99ad
Dawid Twardowski <davidt4323@gmail.com>
2026-02-11 10:34:08 +0100

    chore(web): layout components, move PageTitle to layout, page sizing, add divider

---
+++ apps/web/src/components/layout/page-title.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PageTitle = ({ title }: { title: string }) => {
+  return <h1 className="mb-4 mr-auto mt-6 text-5xl font-medium text-amber-400">{title}</h1>;
+};
--- apps/web/src/components/page-title.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const PageTitle = ({ title }: { title: string }) => {
-  return <h1 className="mb-4 mr-auto text-4xl text-amber-400">{title}</h1>;
-};
