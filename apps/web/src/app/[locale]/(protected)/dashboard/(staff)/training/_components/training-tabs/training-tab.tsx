dc8f8909bb91212f8ca624d5c8cee0922603ce0f
Dawid Twardowski <davidt4323@gmail.com>
2026-02-13 12:45:49 +0100

    feat(web): trainings tabs and tables

---
+++ apps/web/src/app/[locale]/(protected)/dashboard/(staff)/training/_components/training-tabs/training-tab.tsx
@@ -0,0 +1,22 @@
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import React from "react";
+
+import { type TrainingStatus } from "@repo/validators";
+
+import { getQueryClient, trpcServerOptionsProxy } from "@/lib/trpc/client-server";
+import { TrainingTabContent } from "@/app/[locale]/(protected)/dashboard/(staff)/training/_components/training-tabs/training-tab-content";
+
+export const TrainingTab = async ({ statuses }: { statuses: TrainingStatus[] }) => {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery(
+    trpcServerOptionsProxy.training.listStaff.queryOptions({
+      status: statuses,
+    }),
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <TrainingTabContent statuses={statuses} />
+    </HydrationBoundary>
+  );
+};
