0aa36822f06d486fc752b8f564f0073d0b1deeb1
Dawid Twardowski <davidt4323@gmail.com>
2026-02-19 11:34:16 +0100

    feat(web): dashboard panel for staff and customer

--- apps/web/src/app/[locale]/(protected)/dashboard/(staff)/layout.tsx
+++ apps/web/src/app/[locale]/(protected)/dashboard/(staff)/layout.tsx
@@ -4,7 +4,6 @@ import { getSessionUser } from "@/lib/session-user";
 
 export default async function StaffLayout({ children }: { children: React.ReactNode }) {
   const sessionUser = await getSessionUser();
-
   if (sessionUser.isStaff) {
     return <>{children}</>;
   }
--- apps/web/src/app/[locale]/(protected)/dashboard/page.tsx
+++ apps/web/src/app/[locale]/(protected)/dashboard/page.tsx
@@ -1,53 +1,19 @@
 "use server";
 
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 
-import { PageWrapper } from "@/components/layout/page-wrapper";
-import { getQueryClient, trpcServerOptionsProxy } from "@/lib/trpc/client-server";
-import { UserDetails } from "@/app/[locale]/(protected)/dashboard/_components/user-details";
 import { getSessionUser } from "@/lib/session-user";
-
-const links = [
-  {
-    label: "Exercises",
-    href: "/dashboard/exercise",
-  },
-  {
-    label: "Create Exercise",
-    href: "/dashboard/exercise/create",
-  },
-  {
-    label: "Trainings",
-    href: "/dashboard/training",
-  },
-];
+import { StaffDashboard } from "@/app/[locale]/(protected)/dashboard/_components/staff-dashboard";
+import { CustomerDashboard } from "@/app/[locale]/(protected)/dashboard/_components/customer-dashboard";
 
 export default async function DashboardPage() {
   const sessionUser = await getSessionUser();
   if (!sessionUser.isAuthenticated) {
     redirect("/auth/login");
   }
-  const queryClient = getQueryClient();
-  await queryClient.prefetchQuery(trpcServerOptionsProxy.user.me.queryOptions());
+  if (sessionUser.isStaff) {
+    return <StaffDashboard />;
+  }
 
-  return (
-    <PageWrapper title="Dashboard">
-      <ul className="list-disc">
-        {links.map((link) => (
-          <li key={link.href} className="text-blue-400">
-            <Link href={link.href}>{link.label}</Link>
-          </li>
-        ))}
-      </ul>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <div className="mt-8 flex w-full flex-col">
-          <div>
-            <UserDetails />
-          </div>
-        </div>
-      </HydrationBoundary>
-    </PageWrapper>
-  );
+  return <CustomerDashboard />;
 }
