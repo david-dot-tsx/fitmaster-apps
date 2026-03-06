ee935f85ad514ba0fa72868395ee061f785a4b54
Dawid Twardowski <davidt4323@gmail.com>
2026-03-06 15:38:11 +0100

    feat(api): add profile router, create profile route

--- packages/api/src/server/app-router.ts
+++ packages/api/src/server/app-router.ts
@@ -4,10 +4,12 @@ import { auth } from "../routers/auth/auth.route";
 import { exercise } from "../routers/exercise/exercise.route";
 import { training } from "../routers/training/training.route";
 import { trainingDay } from "../routers/trainingDay/training-day.route";
+import { profile } from "../routers/profile/profile.route";
 
 export const appRouter = router({
   user,
   auth,
+  profile,
   exercise,
   training,
   trainingDay,
