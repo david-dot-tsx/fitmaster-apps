c799ac417895f21a282a77028815722a48d745f9
Dawid Twardowski <davidt4323@gmail.com>
2026-03-24 17:50:33 +0100

    feat(mobile): training session finished screen

---
+++ apps/mobile/src/features/trainings/screens/training-session-finished.screen.tsx
@@ -0,0 +1,34 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { router } from "expo-router";
+
+import { ScreenWrapper } from "@/components/layout/screen-wrapper";
+import { Heading } from "@/components/ui/heading";
+import { VStack } from "@/components/ui/vstack";
+import { Button, ButtonText } from "@/components/ui/button";
+
+//TODO: add stats
+export const TrainingSessionFinishedScreen = (_props: {
+  trainingId: string;
+  sessionId: string;
+}) => {
+  return (
+    <ScreenWrapper>
+      <VStack className="flex-1 items-center justify-center gap-4">
+        <Heading size="2xl" className="font-black uppercase italic tracking-tighter text-amber-400">
+          Training Finished<Text className="text-zinc-200">!</Text>
+        </Heading>
+        <View>
+          <Text className="text-center text-zinc-400">Stats</Text>
+        </View>
+        <Button
+          size="lg"
+          className="bg-amber-400 font-bold tracking-widest text-zinc-950"
+          onPress={() => router.replace("/main")}
+        >
+          <ButtonText>Get Back To Main Screen</ButtonText>
+        </Button>
+      </VStack>
+    </ScreenWrapper>
+  );
+};
