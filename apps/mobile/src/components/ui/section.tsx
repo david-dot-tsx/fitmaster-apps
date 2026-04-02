a0086b2af845a3202c0e9c1ab1e7769c7b845dc4
Dawid Twardowski <davidt4323@gmail.com>
2026-04-02 20:55:39 +0200

    feat(mobile): component section

---
+++ apps/mobile/src/components/ui/section.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+
+import { VStack } from "@/components/ui/vstack";
+import { Heading } from "@/components/ui/heading";
+
+interface SectionProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export const Section = ({ title, children }: SectionProps) => {
+  return (
+    <VStack>
+      <Heading size="sm" className=" uppercase tracking-widest text-zinc-400">
+        {title}
+      </Heading>
+      <LinearGradient
+        colors={["#3f3f46", "#3f3f46", "transparent"]}
+        locations={[0, 0.6, 1]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        className="mb-2.5 mt-0.5 h-px w-full rounded-lg"
+      />
+      {children}
+    </VStack>
+  );
+};
