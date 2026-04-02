5af5a4397a6754170d7b05898939893f6a5ae88a
Dawid Twardowski <davidt4323@gmail.com>
2026-04-02 20:42:39 +0200

    feat(mobile): beautify screen-wrapper and pages header

--- apps/mobile/src/components/layout/screen-wrapper.tsx
+++ apps/mobile/src/components/layout/screen-wrapper.tsx
@@ -1,13 +1,22 @@
 import React from "react";
-import { View } from "react-native";
 import { cn } from "@gluestack-ui/utils/nativewind-utils";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { ScreenHeader, type ScreenHeaderProps } from "@/components/layout/screen-header";
 
 export const ScreenWrapper = ({
   children,
   className,
+  header,
 }: {
   children: React.ReactNode;
   className?: string;
+  header?: ScreenHeaderProps;
 }) => {
-  return <View className={cn("size-full flex-1 bg-zinc-950", className)}>{children}</View>;
+  return (
+    <SafeAreaView className={cn("size-full flex-1 bg-zinc-950", className)}>
+      {header && <ScreenHeader {...header} />}
+      {children}
+    </SafeAreaView>
+  );
 };
