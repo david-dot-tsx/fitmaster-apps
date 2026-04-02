cc15a64ecbd837f57695103d1489670070f7bb25
Dawid Twardowski <davidt4323@gmail.com>
2026-04-02 20:37:48 +0200

    chore(mobile): clean up root provider

--- apps/mobile/src/providers/root-provider.tsx
+++ apps/mobile/src/providers/root-provider.tsx
@@ -1,10 +1,8 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useColorScheme as useNativewindColorScheme } from "nativewind";
 import { useEffect } from "react";
 
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { useColorScheme } from "@/hooks/use-color-scheme";
 import { TRPCReactProvider } from "@/lib/trpc/client";
 import { AuthProvider } from "@/providers/auth/auth-context";
 import { FontProvider } from "@/providers/font-provider";
@@ -17,7 +15,6 @@ if (__DEV__) {
 }
 
 export const RootProvider = ({ children }: { children: React.ReactNode }) => {
-  const colorScheme = useColorScheme();
   const nativewindColorScheme = useNativewindColorScheme();
 
   useEffect(() => {
@@ -28,13 +25,11 @@ export const RootProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <GluestackUIProvider>
-        <ThemeProvider value={colorScheme === "light" ? DefaultTheme : DarkTheme}>
-          <TRPCReactProvider>
-            <AuthProvider>
-              <FontProvider>{children}</FontProvider>
-            </AuthProvider>
-          </TRPCReactProvider>
-        </ThemeProvider>
+        <TRPCReactProvider>
+          <AuthProvider>
+            <FontProvider>{children}</FontProvider>
+          </AuthProvider>
+        </TRPCReactProvider>
       </GluestackUIProvider>
     </GestureHandlerRootView>
   );
